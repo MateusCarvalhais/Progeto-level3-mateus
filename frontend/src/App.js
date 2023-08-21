5a50697f6b9092bb676575b4a7afb4f119c0bf7d
MateusCarvalhais <mateus.sardinha.carvalhais@gmail.com>
2023-08-21 10:58:11 -0300

    salvando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Composto from "./componentes/exemplos/Composto"
 function App(){
   return(
     <div>
-      <h1>oi, tudo bem?</h1>
+      <h1>tomate</h1>
       <Simples />
       <Composto />
     </div>
