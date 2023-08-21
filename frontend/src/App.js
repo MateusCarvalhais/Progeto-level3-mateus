716ed782c64742988afa6b4736a411ce142efbbd
MateusCarvalhais <mateus.sardinha.carvalhais@gmail.com>
2023-08-21 10:44:20 -0300

    subindo o projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Simples from "./componentes/exemplos/Simples"
+import Composto from "./componentes/exemplos/Composto"
+function App(){
+  return(
+    <div>
+      <h1>oi, tudo bem?</h1>
+      <Simples />
+      <Composto />
+    </div>
+  )
+}
+
+export default App
